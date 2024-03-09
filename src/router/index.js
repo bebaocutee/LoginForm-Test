66e9bd0aaa1a265ca92f525e9dcaa2e332694032
Tran Xuan Duc <xuduc24122002@gmail.com>
2024-03-09 11:48:34 +0700

    add home component

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import RegisterLoginView from '../views/RegisterLoginView.vue'
+import Home from '../views/Home.vue'
 
 const router = createRouter({
+  mode: "history",
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '',
+      name: 'home',
+      component: Home
+    },
     {
       path: '/dang-nhap',
       name: 'login',
